76a958be34f05d06be687697fad489804bba0d31
sarbjeet <sarbjeet@platform.us>
2025-07-10 18:44:43 +0530

    kanban backend

--- backend/src/project-kanban/project_kanban.controller.ts
+++ backend/src/project-kanban/project_kanban.controller.ts
@@ -6,15 +6,15 @@ import {
   Delete,
   Param,
   Body,
-  UseGuards
-} from '@nestjs/common';
-import { ProjectKanbanService } from './project_kanban.service';
-import { CreateKanbanDto, UpdateKanbanDto } from './project_kanban.dto';
-import { ProjectKanbanDocument } from './project_kanban.interface';
-import { JwtAuthGuard } from 'src/guard/jwt-auth.guard';
-@Controller('project-kanban')
+  UseGuards,
+} from "@nestjs/common";
+import { ProjectKanbanService } from "./project_kanban.service";
+import { CreateKanbanDto, UpdateKanbanDto } from "./project_kanban.dto";
+import { ProjectKanbanDocument } from "./project_kanban.interface";
+import { JwtAuthGuard } from "src/guard/jwt-auth.guard";
+@Controller("api/project-kanban")
 export class ProjectKanbanController {
-  constructor(private readonly kanbanService: ProjectKanbanService) { }
+  constructor(private readonly kanbanService: ProjectKanbanService) {}
   @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() dto: CreateKanbanDto): Promise<{
@@ -23,46 +23,46 @@ export class ProjectKanbanController {
   }> {
     const data = await this.kanbanService.create(dto);
     return {
-      message: 'Kanban column created successfully',
+      message: "Kanban column created successfully",
       data,
     };
   }
   @UseGuards(JwtAuthGuard)
-  @Get(':projectId')
-  async getAll(@Param('projectId') projectId: string): Promise<{
+  @Get(":projectId")
+  async getAll(@Param("projectId") projectId: string): Promise<{
     message: string;
     data: ProjectKanbanDocument[];
   }> {
     const data = await this.kanbanService.findByProject(projectId);
     return {
-      message: 'Kanban columns fetched successfully',
+      message: "Kanban columns fetched successfully",
       data,
     };
   }
   @UseGuards(JwtAuthGuard)
-  @Patch(':id')
+  @Patch(":id")
   async update(
-    @Param('id') id: string,
-    @Body() dto: UpdateKanbanDto,
+    @Param("id") id: string,
+    @Body() dto: UpdateKanbanDto
   ): Promise<{
     message: string;
     data: ProjectKanbanDocument;
   }> {
     const data = await this.kanbanService.update(id, dto);
     return {
-      message: 'Kanban column updated successfully',
+      message: "Kanban column updated successfully",
       data,
     };
   }
   @UseGuards(JwtAuthGuard)
-  @Delete(':id')
-  async remove(@Param('id') id: string): Promise<{
+  @Delete(":id")
+  async remove(@Param("id") id: string): Promise<{
     message: string;
     data: ProjectKanbanDocument;
   }> {
     const data = await this.kanbanService.delete(id);
     return {
-      message: 'Kanban column deleted successfully',
+      message: "Kanban column deleted successfully",
       data,
     };
   }
