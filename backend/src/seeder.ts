14834806e4467e2203ca83a85d65e343957908a1
sarbjeet <sarbjeet.singh@omnyfy.com>
2025-07-12 21:39:38 +0530

    user seeder

---
+++ backend/src/seeder.ts
@@ -0,0 +1,17 @@
+import { seeder } from "nestjs-seeder";
+import { MongooseModule } from "@nestjs/mongoose";
+import { User, UserSchema } from "./user/user.schema";
+import { UserSeeder } from "./user/user.seeder";
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+console.log(process.env.MONGO_URI);
+seeder({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+}).run([UserSeeder]);
---
+++ backend/src/user/user.seeder.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { User } from "./user.schema";
+import { Seeder, DataFactory } from "nestjs-seeder";
+import { checkUserExists, comparePassword, hashPassword } from './user.helper';
+
+@Injectable()
+export class UserSeeder implements Seeder {
+  constructor(@InjectModel(User.name) private readonly user: Model<User>) {}
+
+  async seed(): Promise<any> {
+    // Insert into the database.
+    return this.user.insertMany([
+        {
+            username: "admin",
+            email: "admin@gmail.com",
+            password: await hashPassword("admin"),
+            isActive: true,
+            roles: ["admin"],
+            userRoles: ["admin"],
+            hobbies: ["reading", "writing", "coding"],
+        },
+        {
+          username: "user",
+          email: "user@gmail.com",
+          password: await hashPassword("admin"),
+          isActive: true,
+          roles: ["user"],
+          userRoles: ["user"],
+          hobbies: ["reading", "writing", "coding"],
+      }
+    ]);
+  }
+
+  async drop(): Promise<any> {
+    return this.user.deleteMany({});
+  }
+}
